fa9974458571588415f28eb3094c3b3caced529f
lakshjaisinghani <jaisinghanilaksh@gmail.com>
2022-09-25 12:31:33 +1000

    created a makefile and template directory structure for main() prog

---
+++ making_it_to_main/source/main.c
@@ -0,0 +1,14 @@
+
+// your main code 
+int main(void) { 
+    int val = 0;
+    int not_val;
+
+    while (1) {
+        val += 1;
+
+        if (val == 1000) {
+            not_val = val;
+        }
+    }
+}
